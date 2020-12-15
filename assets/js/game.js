8bc5aa889923d275fd3ec80138aac6ecbb69ac6b
Nate Schroeder <n8dogg59@hotmail.com>
2020-12-15 14:20:56 -0600

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -92,6 +92,13 @@ var startGame = function() {
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
+
+      if ( playerHealth >0 && i < enemyNames.length -1) {
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?")
+        if (storeConfirm) {
+          shop();
+        }
+      }
     }
     // if player isn't alive, stop the game
     else {
@@ -115,5 +122,30 @@ var endGame = function() {
     else {
       window.alert("Thank you for playing Robot Gladiators!  Come back soon!");
     }
+};
+var shop = function() {
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?  Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+  switch (shopOptionPrompt) {
+    case "refill":
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney - 7;
+      break;
+    case "upgrade":
+      window.alert("Upgrading player's attack by 6 for 7 dolllars.");
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+      break;
+    case "leave":
+      window.alert("Leaving the store.");
+      break;
+    default:
+      window.alert("You did not pick a valie option. Try again.");
+      shop();
+      break;
+  }
 };
 startGame();
